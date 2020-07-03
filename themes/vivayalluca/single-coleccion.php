e5eb3bbe8ca39fa0a0f55d1c28d57fe1f1aaddbb
Mario Germán Reyes C <mager19@gmail.com>
2020-07-03 15:14:12 -0500

    progress colecciones.

--- themes/vivayalluca/single-coleccion.php
+++ themes/vivayalluca/single-coleccion.php
@@ -44,20 +44,93 @@ while (have_posts()) : the_post();
 	<section class="imagenes__coleccion pb-5">
 		<div class="container">
 			<div class="row">
-				<div class="col-12 text-center">
-					<?php if (have_rows('imagenes')) : ?>
+				<?php if (have_rows('imagenes')) : ?>
 
-						<?php while (have_rows('imagenes')) : the_row(); ?>
 
-							<?php $imagen = get_sub_field('imagen'); ?>
+					<?php
+					$numrows = count(get_field('imagenes'));
 
-							<img src="<?php echo $imagen['url']; ?>" class="mx-auto">
+					?>
 
-						<?php endwhile; ?>
 
-					<?php endif; ?>
+					<?php
 
-				</div>
+
+					switch ($numrows) {
+						case '1':
+							while (have_rows('imagenes')) : the_row();
+								$imagen = get_sub_field('imagen'); ?>
+								<div class="col-12 text-center">
+									<img src="<?php echo $imagen['url']; ?>" class="mx-auto">
+								</div>
+								<?php
+							endwhile;
+							break;
+
+						case '2':
+							$counter = 1;
+							while (have_rows('imagenes')) : the_row();
+								$imagen = get_sub_field('imagen');
+								if ($counter == 1) { ?>
+									<div class="col-md-7 col-12 mb-4 text-center">
+										<img src="<?php echo $imagen['url']; ?>" class="mx-auto">
+									</div>
+								<?php
+								} else { ?>
+									<div class="col-md-5 col-12 mb-4 text-center d-flex align-items-end">
+										<img src="<?php echo $imagen['url']; ?>" class="mx-auto">
+									</div>
+							<?php 								}
+								$counter++;
+							endwhile;
+							break;
+
+						case '3':
+							$counter = 1;
+							while (have_rows('imagenes')) : the_row();
+
+								if ($counter == 1) {
+									$imagen1 = get_sub_field('imagen');
+								} elseif ($counter == 2) {
+									$imagen2 = get_sub_field('imagen');
+								} else {
+									$imagen3 = get_sub_field('imagen');
+								}
+
+								$counter++;
+							endwhile; ?>
+							<div class="col-md-8 col-12 text-center mb-4 mb-lg-0">
+								<img src="<?php echo $imagen1['url']; ?>" class="mx-auto">
+							</div>
+
+							<div class="col-md-4 col-12 d-flex align-items-end">
+
+								<div class="row">
+									<div class="col-12 mb-4 text-center">
+										<?php echo wp_get_attachment_image($imagen2['id'], array(310, 230)); ?>
+									</div>
+									<div class="col-12 text-center">
+										<?php echo wp_get_attachment_image($imagen3['id'], array(310, 310)); ?>
+									</div>
+								</div>
+
+							</div>
+					<?php
+							break;
+
+						default:
+							# code...
+							break;
+					}
+					?>
+
+					<?php $imagen = get_sub_field('imagen'); ?>
+
+					<!-- <img src="<?php echo $imagen['url']; ?>" class="mx-auto"> -->
+
+
+
+				<?php endif; ?>
 			</div>
 		</div>
 	</section>
