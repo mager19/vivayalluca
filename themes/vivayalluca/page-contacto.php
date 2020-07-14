4d68f40d3819f471afd739a050517977ac636dcf
Mario Germán Reyes C <mager19@gmail.com>
2020-07-14 11:51:43 -0500

    fix contact

--- themes/vivayalluca/page-contacto.php
+++ themes/vivayalluca/page-contacto.php
@@ -40,7 +40,9 @@ get_header();
 	<div class="container-fluid showrooms">
 		<div class="row">
 			<div class="col-12">
-				<h3 class="text-center">Showrooms</h3>
+				<h3 class="title--page text-center">Showrooms</h3>
+
+				<h3 class="show__rooms__subtitle text-center">Comunícate por WhatsApp a tu tienda favorita</h3>
 			</div>
 			<?php while (have_rows('showroomrepetear', 'option')) : the_row(); ?>
 				<div class="col-lg-3 col-md-6 col-12 mx-auto">
@@ -53,9 +55,11 @@ get_header();
 							<h3><?php the_sub_field('nombre_showroom', 'option'); ?></h3>
 							<p><?php the_sub_field('direccion_showroom', 'option'); ?></p>
 							<ul class="links__showroom">
-								<li><a class="telefono__showroom" href="tel:<?php the_sub_field('telefono_showroom', 'option'); ?>"><?php the_sub_field('telefono_showroom', 'option'); ?></a></li>
-								<li><a class="whatsapp__showroom" href="<?php the_sub_field('whatsapp_showroom', 'option'); ?>">
-										<?php the_sub_field('whatsapp_showroom', 'option'); ?></a></li>
+								<li><a class="telefono__showroom" href="tel:<?php the_sub_field('telefono_showroom', 'option'); ?>">
+										<i class="fa fa-phone" aria-hidden="true"></i>
+										<?php the_sub_field('telefono_showroom', 'option'); ?></a></li>
+								<li><a class="whatsapp__showroom" href="https://api.whatsapp.com/send?phone=<?php the_sub_field('whatsapp_showroom', 'option'); ?>&text=%C2%A1Hola!%20%C2%BFEn%20qu%C3%A9%20te%20podemos%20ayudar?" target="_blank">
+										<i class="fa fa-whatsapp" aria-hidden="true"></i><?php the_sub_field('whatsapp_showroom', 'option'); ?></a></li>
 							</ul>
 
 							<a href="mailto:<?php the_sub_field('email'); ?>" class="email__showroom">
